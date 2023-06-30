34ae35e8ce95578796d806032e0488d67cf29bbf
Samrat Ganguly <tanmoydas407@gmail.com>
2023-06-30 22:38:34 +0530

    edit

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "../styles/Loader/Loader.css";
-import { PropagateLoader } from "react-spinners";
+import { PacmanLoader } from "react-spinners";
 
 const Loader = () => {
   return (
     <div className="Loader">
-      <PropagateLoader color={"#313131"} size={35} />
+      <PacmanLoader color={"#313131"} size={30} />
     </div>
   );
 };
